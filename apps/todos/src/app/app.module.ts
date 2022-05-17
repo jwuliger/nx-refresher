193f3106b042673118c3e6e9445feb0e2529aefb
Jared Wuliger <jaredwuliger@gmail.com>
2022-05-17 10:39:45 -0400

    add ng material

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UiModule } from '@myorg/ui';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, HttpClientModule, UiModule],
+    imports: [BrowserModule, HttpClientModule, UiModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
 })
