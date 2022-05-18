0948d05225fc008a0070db865fc275f5a2cd21fc
Jared Wuliger <jaredwuliger@gmail.com>
2022-05-18 13:51:35 -0400

    setup firebase

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -7,6 +7,10 @@ import { UiModule } from '@myorg/ui';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
     declarations: [AppComponent],
@@ -17,7 +21,10 @@ import { MatButtonModule } from '@angular/material/button';
         BrowserAnimationsModule,
 
         MatToolbarModule,
-        MatButtonModule
+        MatButtonModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore())
     ],
     providers: [],
     bootstrap: [AppComponent]
--- apps/todos/src/environments/environment.prod.ts
+++ apps/todos/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+    firebase: {
+        projectId: 'numeracy-journal',
+        appId: '1:42758315443:web:438f7ffd143daacfe23fc6',
+        storageBucket: 'numeracy-journal.appspot.com',
+        locationId: 'us-central',
+        apiKey: 'AIzaSyC277e0E6Wz7GlOwAm8UODF0GscuRcy_G8',
+        authDomain: 'numeracy-journal.firebaseapp.com',
+        messagingSenderId: '42758315443',
+        measurementId: 'G-H5YNE2SDWY'
+    },
     production: true
 };
